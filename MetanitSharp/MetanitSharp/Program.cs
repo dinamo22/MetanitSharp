1b037b170616bc670f4220a6bd4829c266a6a505
Dmitry <dinamo22rus@yandex.ru>
2019-11-16 16:14:10 +0300

    Add project

---
+++ MetanitSharp/MetanitSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MetanitSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.Hello();
+        }
+    }
+
+    class HelloWorld
+    {   
+       public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
