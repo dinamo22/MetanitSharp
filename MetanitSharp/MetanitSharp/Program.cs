f3102ee4f3cdaa0ca70490ba17668ad93470cd95
Dmitry <dinamo22rus@yandex.ru>
2019-12-05 17:46:21 +0300

    #6 Дополнена ClassLibrary_Test
    Пробы с наследованием от класса Person, виртуальные методы, сокрытие, преобразование типов

--- MetanitSharp/MetanitSharp/Program.cs
+++ MetanitSharp/MetanitSharp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MetanitSharp
+﻿using System;
+using ClassLibrary_Test;
+namespace MetanitSharp
 {
     class Program
     {
@@ -29,12 +31,54 @@
             test_users[0, 0] = new Person("Gnome", 42, 654);
             test_users[0, 1] = new Person("Asgorn", 13, 74);
             test_users[0, 2] = new Person("Mporeg", 37, 91);
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 test_users[0, i].GetInfo();
             }
             Person Marbet = test_users[0, 1];
             Marbet.GetInfo();
+            Console.WriteLine();
+
+            ClassLibrary_Test.Person Jack = new ClassLibrary_Test.Person { Name = "qwe" };
+            Jack.Display();
+            Jack = new Employee(Name_is: Jack.Name, "Fucking Slave");
+            ((Employee)Jack).Display(); //downcasting Person to Employee -восходящие преобразование
+
+            Employee employee = new Employee("Tom", "Slave");
+            employee.Display();
+            ClassLibrary_Test.Person person = employee; //upcasting Employee to Person - низходящие преобразования
+            person.Display();
+            ((Employee)person).Display();
+            Employee TryDownCasting = person as Employee;
+            if (TryDownCasting == null)
+            {
+                Console.WriteLine("Преобразование не удалось");
+            }
+            else
+            {
+                TryDownCasting.Display();
+            }
+            Employe TryDownCasting2 = person as Employe;
+            if (TryDownCasting2 == null)
+            {
+                Console.WriteLine("Преобразование не удалось");
+            }
+            else
+            {
+                TryDownCasting2.Display();
+            }
+            Jack.New_Employe(ref Jack,"Big Boss"); // как обратиться к методам Employe если он internal?
+
+            ClassLibrary_Test.Person oleg = new ClassLibrary_Test.Person("oleg");
+            string oleg_string = (string)oleg; //явное преобразование explicit
+            Console.WriteLine(oleg_string);
+            ClassLibrary_Test.Person masha = new ClassLibrary_Test.Person();
+            masha = oleg_string; // неявное implicit
+            masha.Display();
+            ClassLibrary_Test.Person virtual_override_test = new ClassLibrary_Test.Person("Virtual");
+            virtual_override_test.Display();
+            virtual_override_test.New_Employ(ref virtual_override_test,"Microsoft");
+            virtual_override_test.Display();
         }
     }
 }
