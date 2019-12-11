619c753b953d7eb30223320db73567844dc51645
Dmitry <dinamo22rus@yandex.ru>
2019-12-11 15:56:25 +0300

    :D

--- MetanitSharp/MetanitSharp/Program.cs
+++ MetanitSharp/MetanitSharp/Program.cs
@@ -18,7 +18,7 @@ namespace MetanitSharp
             }
             int[] massiv = new int[10];
             Test_Massiv.Test_Delegate(massiv);
-            
+            return 0;
         }
     }
 }
